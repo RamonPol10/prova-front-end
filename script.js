f244e1cd524c79fcd08fc51f7892fb366d5d6106
Ramonpol10 <ramonzinevil369@gmai.com>
2025-04-22 20:47:24 -0300

    Criei variaveis no código js, para que atualize o formulário, com os gastos

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ const totalGastos = document.getElementById('total');
 let gastos = [];
 
 const atualizarLista = () => {
-}
+    listaGastos.innerHTML = '';
+    let total = 0;
+    for (let i = gastos.length - 1; i >= 0; i--) {
+        const gasto = gastos[i];
+        const li = document.createElement('li');
+        let descricaoFormatada = gasto.descricao;
+        let valorFormatado = `R$ ${gasto.valor.toFixed(2)}`;
+
+        if (gasto.valor > 100) {
+            descricaoFormatada = `<span style="color: red; text-decoration: underline;">${gasto.descricao}</span>`;
+            valorFormatado = `<span style="color: red; text-decoration: underline;">R$ ${gasto.valor.toFixed(2)}</span>`;
+        }
+
+        
