e30b5d5e3269ec918fed971e82d861eaaa5a55cc
Ramonpol10 <ramonzinevil369@gmai.com>
2025-04-22 20:35:27 -0300

    Comecei criar o arquivo JS, e colocando as variaveis

---
+++ script.js
@@ -0,0 +1,6 @@
+const formGasto = document.getElementById('form-gasto');
+const descricaoInput = document.getElementById('descricao');
+const valorInput = document.getElementById('valor');
+const categoriaSelect = document.getElementById('categoria');
+const listaDeGastos = document.getElementById('lista-de-gastos');
+const valorTotalSpan = document.getElementById('valor-total');
